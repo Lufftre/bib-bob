990e36c6d965335000c4df2fa98ceb9e2b6e68c2
Ludvig Noring <ludno249@student.liu.se>
2018-01-27 02:06:57 +0100

    initial commit

---
+++ Assets/Player/FastFall.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FastFall : MonoBehaviour {
+
+	public float fallMultiplier = 2.5f;
+	public float lowJumpMultiplier = 2f;
+	public float stopMultiplier = 5f;
+
+	Rigidbody2D rb;
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		if (rb.velocity.y < 0) {
+			rb.gravityScale = fallMultiplier;
+		} else if (rb.velocity.y > 0 && !Input.GetButton("Jump")) {
+			rb.gravityScale = lowJumpMultiplier;
+		} else {
+			rb.gravityScale = 1;
+		}
+
+		if (Mathf.Abs(rb.velocity.x) > 0 && !Input.GetButton("Horizontal")){
+			rb.velocity = new Vector2 (rb.velocity.x * (1 - 1/stopMultiplier), rb.velocity.y);
+		}
+	}
+	
+}
---
+++ Assets/Player/PlayerController.cs
@@ -0,0 +1,104 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+public class PlayerController : NetworkBehaviour {
+
+	Rigidbody2D rb;
+	Mesh mesh;
+	public float moveForce;
+	public float friction;
+	public float jumpVelocity;
+
+	bool grounded = true;
+	Vector2 playerSize;
+	Vector2 boxSize;
+
+	public GameObject bulletPrefab;
+	public Transform bulletSpawn;
+
+	void ColorPlayer(Color frontColor){
+		Color[] c = new Color[24]{
+			Color.black,
+			Color.black,
+			Color.black,
+			Color.black,
+			Color.red,
+			Color.red,
+			frontColor,
+			frontColor,
+			Color.red,
+			Color.red,
+			frontColor,
+			frontColor,
+			Color.blue,
+			Color.blue,
+			Color.blue,
+			Color.blue,
+			Color.yellow,
+			Color.yellow,
+			Color.yellow,
+			Color.yellow,
+			Color.cyan,
+			Color.cyan,
+			Color.cyan,
+			Color.cyan
+		};
+        GetComponent<MeshFilter>().mesh.colors = c;
+
+	}
+	void Awake () {
+		rb = GetComponent<Rigidbody2D>();
+		playerSize = GetComponent<BoxCollider2D>().size;
+		boxSize = new Vector2(playerSize.x, 0.05f);
+		ColorPlayer(Color.white);
+	}
+	
+	// Update is called once per frame
+	void Update(){
+		if (!isLocalPlayer) return;
+
+
+		// Jump
+		Vector2 boxCenter = (Vector2)transform.position;
+		// grounded = Physics2D.OverlapBox()
+		if(Input.GetButtonDown("Jump") && grounded){
+			rb.AddForce(Vector2.up * jumpVelocity, ForceMode2D.Impulse);
+		}
+
+		// Horizontal Move
+		float horizontalInput = Input.GetAxis ("Horizontal");
+		rb.AddForce (Vector2.right * horizontalInput * moveForce, ForceMode2D.Impulse);
+		rb.velocity = new Vector2 (rb.velocity.x * friction, rb.velocity.y);
+
+		// Shoot
+	if (Input.GetKeyDown(KeyCode.F)){
+		CmdFire();
+	}
+
+	}
+	// void FixedUpdate () {
+	public override void OnStartLocalPlayer(){
+		ColorPlayer(Color.green);
+	}
+	// }
+	[Command]
+    void CmdFire()
+    {
+        // Create the Bullet from the Bullet Prefab
+        var bullet = (GameObject)Instantiate(
+            bulletPrefab,
+            bulletSpawn.position,
+            bulletSpawn.rotation);
+
+        // Add velocity to the bullet
+        bullet.GetComponent<Rigidbody2D>().velocity = bullet.transform.forward * 6;
+
+        // Spawn the bullet on the Clients
+        NetworkServer.Spawn(bullet);
+
+        // Destroy the bullet after 2 seconds
+        Destroy(bullet, 2.0f);
+    }
+
+}
