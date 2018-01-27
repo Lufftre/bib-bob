de7415923da47fc2c9ac280481d733ab7e70c8ed
Ludvig Noring <ludno249@student.liu.se>
2018-01-27 05:02:18 +0100

    😴

---
+++ Assets/CameraFollow.cs
@@ -0,0 +1,26 @@
+﻿ using UnityEngine;
+ 
+ public class CameraFollow : MonoBehaviour
+ {
+     public Transform playerTransform;
+     public int depth = -20;
+ 
+	public float smoothSpeed;
+	public Vector3 offset;
+
+	void FixedUpdate ()
+	{
+		if (playerTransform == null) return;
+
+		Vector3 desiredPosition = playerTransform.position + offset;
+		Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed * Time.deltaTime);
+		transform.position = smoothedPosition;
+
+		transform.LookAt(playerTransform);
+	}
+ 
+     public void setTarget(Transform target)
+     {
+         playerTransform = target;
+     }
+ }
